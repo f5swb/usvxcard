a3dd84dc33c0f0ac3ece1d512ed87bfc12204d92
Dimitri <35607641+f5swb@users.noreply.github.com>
2020-03-30 10:39:15 +0200

    Add files via upload

---
+++ sound.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# f5swb@hotmail.com (2020)
+
+amixer -c 0 set 'Headphone' -M 50%
+amixer -c 0 set 'Speaker' -M 0%
+amixer -c 0 set 'Speaker AC' -M 0%
+amixer -c 0 set 'Speaker DC' -M 0%
+amixer -c 0 set 'Playback' -M 50%
+amixer -c 0 set 'Capture' -M 50%
+amixer -c 0 set '3D' -M 0%
+amixer -c 0 set 'ADC PCM' -M 41%
+amixer -c 0 set 'ALC Attack' -M 0%
+amixer -c 0 set 'ALC Decay' -M 0%
+amixer -c 0 set 'ALC Max Gain' -M 0%
+amixer -c 0 set 'ALC Min Gain' -M 0%
+amixer -c 0 set 'ALC Hold Time' -M 0%
+amixer -c 0 set 'ALC Max Gain' -M 0%
+amixer -c 0 set 'ALC Min Gain' -M 0%
+amixer -c 0 set 'ALC Target' -M 0%
+amixer -c 0 set 'Left Input Boost Mixer LINPUT1' -M 32%
+amixer -c 0 set 'Left Input Boost Mixer LINPUT2' -M 0%
+amixer -c 0 set 'Left Input Boost Mixer LINPUT3' -M 0%
+amixer -c 0 set 'Left Output Mixer Boost Bypass' -M 0%
+amixer -c 0 set 'Left Input Boost Mixer LINPUT3' -M 0%
+amixer -c 0 set 'Noise Gate Threshold' -M 0%
+amixer -c 0 set 'Right Input Boost Mixer RINPUT1' -M 0%
+amixer -c 0 set 'Right Input Boost Mixer RINPUT2' -M 0%
+amixer -c 0 set 'Right Input Boost Mixer RINPUT3' -M 0%
+amixer -c 0 set 'Right Output Mixer Boost Bypass' -M 0%
+amixer -c 0 set 'Right Output Mixer PCM' -M 0%
+amixer -c 0 set 'Right Output Mixer RINPUT3' -M 0%
+
+
